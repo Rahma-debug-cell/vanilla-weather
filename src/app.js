70b11d8507ba4fba265d4c4e451869c6e5843a99
Rahma-debug-cell <rahmaruun2002@gmail.com>
2023-11-26 20:56:22 -0600

    added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+
+}
+
+
+
+function searchCity(city){
+    let apiKey = "90fb196obc3c9904fadt680fa8296a72";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+
+
+}
+
+
+
+
+
+
 function handlesearchsubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handlesearchsubmit);
+
+searchCity("Dubai");
