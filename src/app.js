e0b176d231242094b0ee5fd6b104ddf50ec30cd8
Rahma-debug-cell <rahmaruun2002@gmail.com>
2023-11-27 09:46:58 -0600

    injected URL icon

--- src/app.js
+++ src/app.js
@@ -7,19 +7,23 @@ function refreshWeather(response){
     let windspeedElement = document.querySelector("#wind-speed");
      let timeElement = document.querySelector("#time");
      let date = new Date(response.data.time * 1000);
-
+     let iconElement = document.querySelector("#icon");
+     
     
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+
 
 }
 
 function formatDate(date) {
     let minutes = date.getMinutes();
-    let hours = date.getHourse();
+    let hours = date.getHours();
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fridy", "Saturday"];
 
     let day = days [date.getDay()];
@@ -33,9 +37,6 @@ function formatDate(date) {
 
 
 
-
-
-
 function searchCity(city){
     let apiKey = "90fb196obc3c9904fadt680fa8296a72";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -43,9 +44,6 @@ function searchCity(city){
 
 
 }
-
-
-
 
 
 
